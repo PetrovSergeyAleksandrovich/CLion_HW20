ce23e3de89cf79948459e406e077374638c76edb
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-19 12:29:06 +0300

    HW_20 task_1 done

--- HW20_task_1.cpp
+++ HW20_task_1.cpp
@@ -9,7 +9,7 @@ void swap(int* ptr_x, int* ptr_y)
 
 int main()
 {
-    int a = 10, b = 20;
+    int a = 1, b = 100;
     std::cout << "Before: " << a << " " << b << std::endl;
     swap(&a, &b);
     std::cout << "After: " << a << " " << b;
