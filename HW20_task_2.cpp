667ff928401f500f8de57ab95aebef325eadb5ec
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-19 20:28:04 +0300

    HW_20 task_2 done; need to optimize sorting with pointer;

---
+++ HW20_task_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int* rotate_list(int* ptr)
+{
+    int tmp_list[10]={};
+
+    for(int i = 0 ; i < 10; i++)
+    {
+        tmp_list[i] = *(ptr+9-i);
+    }
+
+    for(int i = 0 ; i < 10; i++)
+    {
+        *(ptr+i) = tmp_list[i];
+    }
+    return tmp_list;
+}
+
+
+int main()
+{
+    int list[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout << list+i << " " << *(list+i) << std::endl;
+    }
+
+    rotate_list(list);
+
+    std::cout << std::endl;
+
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout << list+i << " " << *(list+i) << std::endl;
+    }
+
+}
